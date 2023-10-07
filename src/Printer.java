fd4ad6e09c9da5ffccde9e4b43573666e0a62284
sonali23000 <sonalimitua@gmail.com>
2023-10-07 20:50:29 +0300

    two comment added

--- src/Printer.java
+++ src/Printer.java
@@ -12,9 +12,11 @@ public class Printer {
 		        System.out.print("This app is made by: ");
 
 		        // 4.
+		        // Declare String variable.
 		        String creator = "Sonali Mitua";
 
 		        // 5.
+		        // Print "Creator" to the console.
 		        System.out.println(creator);
 
 		        // 6. Declare two numeric variables 
@@ -24,10 +26,12 @@ public class Printer {
 		        // 7. First, try to print the value of number1 to the console. When done, comment the line.
 		        // System.out.println(number1);
 
-		        // 8. 
+		        // 8.
+		        //Print "The value of number1" to the console
 		        System.out.println("The value of number1 is " + number1);
 
 		        // 9.
+		       // Prínt "The value of number2" to the console
 		        System.out.println("The value of number2 is " + number2);
 
 		        // 10. Declare an integer typed variable called multiplication.
