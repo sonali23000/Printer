2c40dd030f72b8ab3999d8604f3252185989137a
sonali23000 <sonalimitua@gmail.com>
2023-10-07 20:20:21 +0300

    Initial commit

---
+++ src/Printer.java
@@ -0,0 +1,58 @@
+
+public class Printer {
+
+	public static void main(String[] args) {
+		
+
+		    
+		        // 2. Print Project to the console.
+		        System.out.println("Printer App");
+
+		        // 3.Print method
+		        System.out.print("This app is made by: ");
+
+		        // 4.
+		        String creator = "Sonali Mitua";
+
+		        // 5.
+		        System.out.println(creator);
+
+		        // 6. Declare two numeric variables 
+		        int number1 = 5;
+		        int number2 = 2;
+
+		        // 7. First, try to print the value of number1 to the console. When done, comment the line.
+		        // System.out.println(number1);
+
+		        // 8. 
+		        System.out.println("The value of number1 is " + number1);
+
+		        // 9.
+		        System.out.println("The value of number2 is " + number2);
+
+		        // 10. Declare an integer typed variable called multiplication.
+		        int multiplication;
+
+		        // 11. Multiply number1 and number2 .
+		        multiplication = number1 * number2;
+
+		        // 12. Print “5 * 2 = 10” to the console.
+		        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+
+		        // 13. Repeat 10-12 for addition, division, and subtraction.
+		        int addition = number1 + number2;
+		        double division = (double) number1 / number2; // Casting to double for accurate division
+		        int subtraction = number1 - number2;
+
+		        System.out.println(number1 + " + " + number2 + " = " + addition);
+		        System.out.println(number1 + " / " + number2 + " = " + division);
+		        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+
+		        
+		        
+		    }
+	
+
+	}
+
+
